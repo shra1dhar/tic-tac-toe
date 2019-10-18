e0e84a3b465d579669752a7d620da6ebaeff11f6
Gaurav Sharma <gauravsharmags06@gmail.com>
2019-10-18 21:36:16 +0530

    Tic Tac Toe History Bug Fixed

---
+++ src/components/Board.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import Square from "./Square";
+
+class Board extends Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    //   const rows = 3;
+    //   const cols = 3;
+    return (
+      <div>
+          {/* {for(let i=0; i< rows; i++)} */}
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Board;
